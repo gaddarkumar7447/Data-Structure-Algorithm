ed9516b4b3875f2315c13b90841bafb6a8094dc4
Sumit Mishra <sumitmishra9963@gmail.com>
2022-01-10 19:57:27 +0530

    added 2 new question from leet code

--- src/main/java/code/missiongoogle/ArrayQuestions.java
+++ src/main/java/code/missiongoogle/ArrayQuestions.java
@@ -3,10 +3,12 @@ package code.missiongoogle;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ArrayQuestions {
     public static void main(String[] args) {
-        int [] intArray= new int[]{5, 23, 28, 2, 40};
+        int [] intArray= new int[]{2,7,11,15};
         String [] stringArray = new String[]{"Sumit","Gaddar","Bhulan","Ramu"};
 
         /*Array-Traversal*/
@@ -15,16 +17,97 @@ public class ArrayQuestions {
         //gfgUsingCollection(intArray);
 
         /*Array Sort Check*/
-        //System.out.println(mySolutionSortCheck(false,intArray));
+       // System.out.println(mySolutionSortCheck(true,intArray));
+
         //TODO: check with recursive method(GFG solution)
 
         /*Find Maximum in array*/
-        System.out.println(String.valueOf(mySolutionMaximumInArray(intArray)));
-        
+       // System.out.println(String.valueOf(mySolutionMaximumInArray(intArray)));
+
+     //  AshuArrays.reverseArray(intArray);
+       //AshuArrays.sortCheck(intArray);
+
+        /*Leet-code Problems*/
+        /*Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+        * Example 1:
+
+        Input: nums = [1,2,3,1], k = 3
+        Output: true
+        Example 2:
+
+        Input: nums = [1,0,1,1], k = 1
+        Output: true
+        Example 3:
+
+        Input: nums = [1,2,3,1,2,3], k = 2
+        Output: false
+
+
+        Constraints:
+
+        1 <= nums.length <= 105
+        -109 <= nums[i] <= 109
+        0 <= k <= 105*/
+
+        //System.out.println(containsDuplicate(1,intArray));
+
+
+
+        /**/
+        pairSum(intArray,9);
+
+
+
+
+
+
+
+
+
+    }
+
+    private static void pairSum(int[] intArray,int target) {
+        /*2,7,11,15*/
+        String indexes=null;
+        HashMap<Integer, Integer> indexVal = new HashMap<>();
+        for(int i=0;i<intArray.length;i++){
+            int diff = target-intArray[i];
+            if(indexVal.containsKey(diff)){
+                indexes="first: "+indexVal.get(diff)+" second:"+i;
+                break;
+            }
+            indexVal.put(intArray[i],i);
+        }
+        System.out.println(indexes);
+
+
+
+    }
+
+
+    private static boolean containsDuplicate(int k, int[] intArray) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        for(int num : intArray) {
+            if(map.containsKey(num) && isIndexInRange(i, map.get(num), k)) {
+                return true;
+            }
+            map.put(num, i);
+            i++;
+        }
+        return false;
+
+    }
+
+
+
+    private static boolean isIndexInRange(int curr_index, int map_index, int k) {
+        int x = Math.abs(curr_index - map_index);
+        return x <= k ? true : false;
     }
 
     private static int mySolutionMaximumInArray(int[] intArray) {
-        if(intArray.length!=0 && intArray.length==1){
+        if(intArray.length == 1){
            return intArray[0];
         }else {
             int maxSoFar=-1;
@@ -42,7 +125,6 @@ public class ArrayQuestions {
         if(arr.length==0){
             return false;
         }else {
-            int curr;
             if(isAscending){
                 for (int i=1;i<arr.length;i++){
                     if(arr[i-1]>arr[i])
@@ -101,10 +183,11 @@ public class ArrayQuestions {
                     intArray[start]=intArray[start]*intArray[end];
                     intArray[end]= intArray[start]/intArray[end];
                     intArray[start]= intArray[start]/intArray[end];
+
                 }catch (ArithmeticException e){
-                    int temp = intArray[start];
+                    /*int temp = intArray[start];
                     intArray[start]=intArray[end];
-                    intArray[end]= temp;
+                    intArray[end]= temp;*/
                 }
 
                 System.out.println("start> "+start+" end>"+end);
