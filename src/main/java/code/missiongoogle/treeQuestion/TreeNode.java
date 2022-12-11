e4c7000a7b436fe3387b15900f7ebc0cbb939c29
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-12-11 18:18:56 +0530

    Add some files

---
+++ src/main/java/code/missiongoogle/treeQuestion/TreeNode.java
@@ -0,0 +1,22 @@
+package code.missiongoogle.treeQuestion;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int maxProduct(TreeNode root) {
+
+        return  0;
+    }
+}
