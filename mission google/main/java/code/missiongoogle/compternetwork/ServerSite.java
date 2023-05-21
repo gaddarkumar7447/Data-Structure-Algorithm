015d46846ba3a9bd94a085dff60c76695c284753
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2023-05-21 23:36:17 +0530

    socket programming

--- mission google/main/java/code/missiongoogle/compternetwork/ServerSite.java
+++ mission google/main/java/code/missiongoogle/compternetwork/ServerSite.java
@@ -1,2 +1,25 @@
-package code.missiongoogle.compternetwork;public class ServerSite {
+package code.missiongoogle.compternetwork;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerSite {
+    public static void main(String []args){
+        Scanner sv = new Scanner(System.in);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            ServerSocket ss = new ServerSocket(6666);
+            Socket s = ss.accept();
+            DataInputStream dataInputStream = new DataInputStream(s.getInputStream());
+            String str = dataInputStream.readUTF();
+            System.out.println("message: "+str);
+            ss.close();
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
 }
